007ccce9e3459e2f0a9f003a3a4a6d6ba3cd0576
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2022-12-02 15:50:17 -0500

    small bug fixes

--- src/components/Create.js
+++ src/components/Create.js
@@ -35,7 +35,7 @@ export default function Create() {
     return (
         <div class="relative flex min-h-screen text-gray-800 antialiased flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
             <div class="relative py-3 sm:w-96 mx-auto text-center">
-                <span class="text-2xl font-light ">Update employee</span>
+                <span class="text-2xl font-light ">Add new employee</span>
                 <div class="mt-4 bg-white shadow-md rounded-lg text-left">
                     <div class="h-2 bg-purple-400 rounded-t-md"></div>
                     <div class="px-8 py-6 ">
